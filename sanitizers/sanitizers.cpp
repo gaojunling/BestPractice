46e69e5baeebd1c253e0f756bdd9328e50420d78
gaojunling <gaojunling518@126.com>
2024-04-19 18:14:43 +0800

    添加项目文件。

---
+++ sanitizers/sanitizers.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <ostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+int main(int argc, const char* argv[]) {
+	std::cout << "..." << std::endl;
+	std::cout << "string is :";
+	char* s = (char*)malloc(100);
+	strcpy_s(s, strlen("Hello world!") + 1, "Hello world!");
+	std::cout << s << std::endl;
+	return 0;
+}
